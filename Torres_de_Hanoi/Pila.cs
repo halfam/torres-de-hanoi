b38b25eb05f3b7c557f8f757e4d0fd306c58f030
Joan <joanab1999@gmail.com>
2022-02-21 19:00:15 +0100

    Implementada la clase Pila

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*public Pila() que representa el constructor de la clase.
+public void push(Disco d) que permitir´a colocar un disco en la pila de discos.
+public Disco pop() que permitir´a extraer el disco de la parte superior de la pila.
+public bool isEmpty() que informar´a si la pila est´a o no vac´ıa.
+Size para representar la cantidad de discos que hay en el palo.
+Top para representar el disco que est´a en la parte superior del palo.
+Elementos para representar el conjunto de discos que hay en ese palo
+*/
 namespace Torres_de_Hanoi
 {
     class Pila
     {
-        public int Size { get; set; }
+        public int Size
+        { 
+            get 
+            {
+                return Elementos.Count;
+            }
+            set;
+        }
+
         /* TODO: Elegir tipo de Top
         public int Top { get; set; }
         public String Top { get; set; }        
@@ -17,26 +33,51 @@ namespace Torres_de_Hanoi
         public Disco[] Elementos { get; set; }
         public List<Disco> Elementos { get; set; }
         */
+        public Disco Top 
+        {
+			get 
+            {
+                return Elementos.Last;
+            }
+            set;
+        }
 
+        public List<Disco> Elementos { get; set; }
         /* TODO: Implementar métodos */
         public Pila()
         {
-
+            Elementos = new List<Disco>();
+        }
+        public Pila(List<Disco> discos)
+        {
+            Elementos = discos;
         }
 
         public void push(Disco d)
         {
-
+            if (Top.Valor > d.Valor )
+	        {
+                Elementos.Add(d);
+	        }
         }
 
         public Disco pop()
         {
-            return null;
+            if (this.isEmpty())
+	        {
+                return null;
+	        }
+            Disco elem = Top;
+            Elementos.Remove(elem);
+            return elem;
         }                
 
         public bool isEmpty()
         {
-            return true;
+            if (Size == 0)
+                return true;
+            else
+                return false;
         }
 
     }
